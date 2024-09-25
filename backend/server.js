07600915c9cd1d8fe41d4a455fc2bb2836e9d7eb
imjustha <imjustha@gmail.com>
2024-09-25 17:22:02 -0500

    Logout function that clear cookies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 
 import { connectDB } from "./lib/db.js";
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // allows you to parse the body of the request
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
