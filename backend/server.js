4490f6960a3935186caf2d483e3096f139806558
imjustha <imjustha@gmail.com>
2024-10-11 00:43:29 -0500

    Update product routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +16,7 @@ app.use(express.json()); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
     console.log("server running on port http://localhost:" + PORT);
