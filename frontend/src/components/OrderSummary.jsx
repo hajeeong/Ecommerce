e4e99968e47337ddb26e8a26b2c00dab89e6cb09
imjustha <imjustha@gmail.com>
2024-10-20 14:04:04 -0500

    Update Stripe payment method

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51KK9geLf9hQrhgXbjftPj3b015hHzJwYAStLyRwTVWelYqPjw3mapjNxAVtuSPV6HMxdCE4l6Ag0SpDm5AW58uhW007gwpFvqr"
 );
 
 const OrderSummary = () => {
